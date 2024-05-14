9454b1062469a6a87090a91fdecd4561eb6d9513
Paul Derscheid <paulderscheid@gmail.com>
2024-05-14 20:51:51 +0200

    feat: add robots.txt and sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  site: 'https://paulderscheid.xyz',
+  integrations: [mdx(), sitemap()]
 });
