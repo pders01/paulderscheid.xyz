1d94d25d50136a0fc006f9ea852f8052a80c5b5b
pders01 <paulderscheid@gmail.com>
2025-08-09 00:07:33 +0200

    refactor: migrate to tailwind + basecoat
    - Add .prettierrc.
    - Reformat src/.
    - Add @astrojs/sitemap, generate on build.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   site: 'https://paulderscheid.xyz',
-  integrations: [mdx(), sitemap()]
+  integrations: [mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
